e55ecb26af26196b664f4019e4ca464f87e64bc7
LiubovAnufriieva <149685540+LiubovAnufriieva@users.noreply.github.com>
2024-04-08 11:37:16 +0000

    Deploying to gh-pages from @ LiubovAnufriieva/11-practic@559c0c33298ee56c20734f3cdc69b61aa1a2355f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,30 +1,8 @@
-import{S as m,i as l}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();function p(o){const r=document.querySelector(".gallery"),i=o.map(({webformatURL:e,largeImageURL:t,tags:a,likes:u,views:d,comments:h,downloads:f})=>`
-  
-        <div class="gallery-card">
-            <li class="gallery_items">
-                <a href="${t}">
-                <img src="${e}" alt="${a}" class="gallery-img"></a>
-                <div class="card-text-div">
-                    <ul class="card-text-list">
-                        <li class="card-text">
-                            <h2>Likes</h2>
-                            <p>${u}</p>
-                        </li>
-                        <li class="card-text">
-                            <h2>Views</h2>
-                            <p>${d}</p>
-                        </li>
-                        <li class="card-text">
-                            <h2>Comments</h2>
-                            <p>${h}</p>
-                        </li>
-                        <li class="card-text">
-                            <h2>Downloads</h2>
-                            <p>${f}</p>
-                        </li>
-                    </ul>
-                </div>
-                
-            </li>
-        </div>`).join("");r.insertAdjacentHTML("afterbegin",i),new m(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom",widthRatio:.9,heightRatio:.8}).refresh()}function y(o){const r="43230635-158e2f6795128fbec19d81d21",i="https://pixabay.com/api/",s=new URLSearchParams({key:r,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`${i}?${s}`).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()}).then(e=>{e.total||l.error({title:"Error",position:"center",message:"Sorry, there are no images matching your search query. Please try again!"}),p(e.hits)}).catch(e=>{l.error({title:"Error",position:"topRight",message:"Oops! Something went wrong!"})}).finally(()=>loader.hidden=!0)}const n=document.querySelector(".search-form"),g=document.querySelector(".gallery"),c=document.querySelector(".loader");n.addEventListener("submit",L);c.hidden=!0;function L(o){o.preventDefault(),g.innerHTML="",c.hidden=!1;const{searchRequest:r}=o.currentTarget.elements;let i=r.value;y(i),n.reset()}
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))n(o);new MutationObserver(o=>{for(const s of o)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function e(o){const s={};return o.integrity&&(s.integrity=o.integrity),o.referrerPolicy&&(s.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?s.credentials="include":o.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(o){if(o.ep)return;o.ep=!0;const s=e(o);fetch(o.href,s)}})();const c="http://localhost:3000/todos",d=document.querySelector(".todo__form"),i=document.querySelector(".list");d.addEventListener("submit",f);i.addEventListener("click",h);i.addEventListener("click",p);async function a(t,r={}){const e=await fetch(t,r);if(!e.ok)throw new Error(e.statusText);return await e.json()}a(c).then(t=>i.insertAdjacentHTML("beforeend",u(t))).catch(t=>console.log(t));function u(t){return t.map(({id:r,title:e,completed:n})=>`
+        <li data-id="${r}" class="list__item">
+            <input type="checkbox" class="list__checkbox" ${n&&"checked"}>
+            <h2 class="list__title">${e}</h2>
+            <button class="list__btn">x</button>
+        </li>
+    `).join("")}function f(t){t.preventDefault();const{todo:r}=t.currentTarget.elements;a(c,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({title:r.value,completed:!1})}).then(e=>i.insertAdjacentHTML("beforeend",u([e]))).catch(e=>alert(e)).finally(()=>d.reset())}function h(t){if(!t.target.classList.contains("list__checkbox"))return;t.preventDefault();const e=t.target.closest(".list__item").dataset.id;a(`${c}/${e}`,{method:"PATCH",body:JSON.stringify({completed:t.target.checked})}).then(n=>t.target.checked=n.completed).catch(n=>console.log(n))}function p(t){if(!t.target.classList.contains("list__btn"))return;const r=t.target.closest(".list__item"),e=r.dataset.id;a(`${c}/${e}`,{method:"DELETE"}).then(n=>r.remove()).catch(n=>alert(n))}
 //# sourceMappingURL=commonHelpers.js.map
