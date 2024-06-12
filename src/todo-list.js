1f65371e63be153ab78c6af321fe7caa45dde007
Lyuzika <lyuzika@gmail.com>
2024-06-12 19:51:40 +0300

    markup add

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,34 +1,5 @@
 import axios from 'axios';
-// import "./css/styles.css"
-
-// const fetchUsers = async () => {
-// 	const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-
-
-    
-// 	return response.data;
-
-
-    
-// };
-
-// fetchUsers()
-// 	.then(users => console.log(users));
-
-//     console.log("Before try...catch");
-
-//     try {
-//         const result = 10 / 0;
-//         console.log(result); // Цей рядок не виконається через помилку
-//       } catch (error) {
-//         // Обробимо помилку
-//         console.error(error.message);
-//       }
-      
-//       console.log("After try...catch");
-
-
-      import "./css/styles.css"
+import "./css/styles.css"
 
 const BASE_URL = "http://localhost:3000/todos"
 
